37eb6749aa0aad9fb1e4e999594f2494fc8a18ec
Quintin Russell <quinn.j.russell@gmail.com>
2021-11-23 22:04:50 -0800

    initial setup

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import parseRoute from '../client/parse-route';
 import Home from './pages/home';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
   render() {
     return <Home />;
   }
---
+++ client/components/new-expense.jsx
@@ -0,0 +1,7 @@
+// import React from 'react';
+
+// // make if/else for if it's a new or past expense
+
+// class ExpenseForm extends React.Component {
+
+// }
