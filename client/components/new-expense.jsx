d9acbe0a3178d7116f3b88d90c05998fb32b3d21
Quintin Russell <quinn.j.russell@gmail.com>
2021-11-24 20:18:19 -0800

    testing html for heading

--- client/components/new-expense.jsx
+++ client/components/new-expense.jsx
@@ -1,7 +1,20 @@
 // import React from 'react';
 
+// const styles = {
+
+// };
+
 // // make if/else for if it's a new or past expense
 
 // class ExpenseForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       pastExpenses: []
+//     };
+//   }
 
+//   render() {
+
+//   }
 // }
