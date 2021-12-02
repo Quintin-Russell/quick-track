c17d2597366f86e65359c896c6717df954c5e657
Quintin Russell <quinn.j.russell@gmail.com>
2021-12-01 19:16:16 -0800

    compleded Menu component

--- client/components/menu.jsx
+++ client/components/menu.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-// figure out how to close menu after choosing an option
-
 export default function Menu(props) {
   return (
     <div className="overlay">
       <div className="menu-cont">
         <div className="header-cont">
             <i onClick={props.toggleMenu} className="fas fa-angle-double-left"></i>
-            <a className="header-logo-cont" href="#">
+          <a onClick={props.toggleMenu} className="header-logo-cont" href="#">
               <img className="menu-header-logo" src="images/quick-track-logo.png" alt="quick-track-logo"></img>
             </a>
           <h1 className="menu-txt">
@@ -19,7 +17,7 @@ export default function Menu(props) {
           {props.pages.map(page => {
             if (page.menuIcon) {
               return (
-                <a className="menu-header-cont" key={page.hash} href={page.hash}>
+                <a onClick={props.toggleMenu} className="menu-header-cont" key={page.hash} href={page.hash}>
                   <img className='menu-icon-img' src={page.imgSrc} alt={page.imgAlt} />
                   <p className="pg-id-txt">{page.name}</p>
                 </a>
